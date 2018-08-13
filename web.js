ec7c431eabb2e49b16bbe84adda4761c6b1d0720
takeshiaoyagijtb <39432011+takeshiaoyagijtb@users.noreply.github.com>
2018-08-13 11:01:35 +0900

    Update web.js

--- web.js
+++ web.js
@@ -1,10 +1,19 @@
+var cool = require('cool-ascii-faces');
 var express = require('express');
+var app = express();
 
-var app = express.createServer();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-	res.send('Hello World');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/test', function(req, res){
+	res.set('Content-Type', 'text/html');
+	res.send('<html><head></head><body>js</body></html>');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port);
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
